7c73e26e0566b20dedaa557fd0cbecf00d56bb99
Lennart Wisbar <anlennart@gmail.com>
2017-02-18 23:20:24 +0100

    Constants, unary operations and clear functionality added.
    Next: binary operations.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+struct CalculatorBrain {
+    private var accumulator: Double?
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+    private enum Operation {
+        case constant(Double)
+        case unary((Double) -> Double)
+        case binary((Double, Double) -> Double)
+    }
+    
+    private let operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "sin": Operation.unary(sin),
+        "cos": Operation.unary(cos),
+        "tan": Operation.unary(tan),
+        "√": Operation.unary(sqrt),
+        "±": Operation.unary({ -$0 }),
+        "×": Operation.binary(*),
+        "÷": Operation.binary(/),
+        "+": Operation.binary(+),
+        "−": Operation.binary(-),
+        "^": Operation.binary(pow)
+    ]
+    
+    mutating func performOperation(_ symbol: String) {
+        guard let operation = operations[symbol] else {
+            return
+        }
+        switch operation {
+        case .constant(let value):
+            accumulator = value
+        case .unary(let function):
+            if accumulator != nil {
+                accumulator = function(accumulator!)
+            }
+        case .binary(let function):
+            break
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,16 +11,45 @@ import UIKit
 class ViewController: UIViewController {
 
     private var userIsInTheMiddleOfTyping = false
+    private var brain = CalculatorBrain()
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
     
     @IBOutlet weak var display: UILabel!
     
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
 
     @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        brain.performOperation(sender.currentTitle!)
+        if let result = brain.result {
+            displayValue = result
+        }
     }
 
     @IBAction func clear(_ sender: UIButton) {
+        brain = CalculatorBrain()
+        display.text = "0"
+        userIsInTheMiddleOfTyping = false
     }
 }
 
